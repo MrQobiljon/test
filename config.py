5b4d91b06cd89faebc902ed5fc1fe3ae812073b2
MrQobiljon <madrahimovq@gmail.com>
2022-10-24 13:24:23 +0500

    first commmit

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+BU YERDA SIZGA KERAKLI BO'LADIGAN TOKEN VA SHUNGA O'XSHAGAN MA'LUMOTLAR SAQLANADI
+"""
+TOKEN = '5385510421:AAEw14Fi_P1kRPvqQfe_4GIzyzzREX0Ue20'
+
+PROVIDER_TOKEN = "371317599:TEST:1666262182843"
+ADMINS = '''Adminlarni saqlash uchun o'zgaruvchi, listdaham saqlasa bo'ladi'''
+
+
+DB_HOST = 'ec2-3-220-207-90.compute-1.amazonaws.com'
+DB_NAME = 'd8ki8h5u1d7euj'
+DB_USER = 'ifvograglrswfq'
+DB_PORT = '5432'
+DB_PASSWORD = '2d14ca74aaa6957a0600a9cd2131533a3f06fbdfef37dfe791a2307ed6cd629a'
+
