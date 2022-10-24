08cdee524a6226e85a521bfe01dab18db04631cb
Qobiljon <86218810+MrQobiljon@users.noreply.github.com>
2022-10-24 13:29:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ PROVIDER_TOKEN = "371317599:TEST:1666262182843"
 ADMINS = '''Adminlarni saqlash uchun o'zgaruvchi, listdaham saqlasa bo'ladi'''
 
 
-DB_HOST = 'ec2-3-220-207-90.compute-1.amazonaws.com'
-DB_NAME = 'd8ki8h5u1d7euj'
-DB_USER = 'ifvograglrswfq'
+DB_HOST = 'ec2-52-208-164-5.eu-west-1.compute.amazonaws.com'
+DB_NAME = 'd508qcp95cfesm'
+DB_USER = 'qbhhrkzeeixqtp'
 DB_PORT = '5432'
-DB_PASSWORD = '2d14ca74aaa6957a0600a9cd2131533a3f06fbdfef37dfe791a2307ed6cd629a'
+DB_PASSWORD = 'b9d2285ce1bbde3683d14ed184800ee03c5dd69f626fce699ca0247b6e5cf002'
 
